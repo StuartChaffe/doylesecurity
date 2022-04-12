c81c1fa8c0d85065442b421b4e9573b91826c311
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-04-12 21:59:11 +0100

    Add text to header

--- wp-content/themes/doyle/partials/site-header.php
+++ wp-content/themes/doyle/partials/site-header.php
@@ -2,9 +2,13 @@
 	<div class="site-header__inner">
 		<h1 class="site-header__logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/src/images/logo.svg" alt="Doyle Security logo" /></a></h1>
 		<button class="site-header__menu-btn" data-toggle="#mobile-menu" data-toggle-body-class="nav-open" aria-label="Toggle mobile menu" aria-expanded="false" aria-controls="mobile-menu">Mobile Menu</button>
-		<nav class="site-header__nav">
-			<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
-		</nav>
+
+		<div class="site-header__content">
+			<p>Need a Security Solution call 01226 298492</p>
+			<nav class="site-header__nav">
+				<?php wp_nav_menu( array('theme_location' => 'primary') ); ?>
+			</nav>
+		</div>
 	</div>
 </header>
 
