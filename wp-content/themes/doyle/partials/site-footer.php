31581132d846cea5fe3bb3fb337a44a5e4df7962
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-04-18 20:49:45 +0100

    Style footer

--- wp-content/themes/doyle/partials/site-footer.php
+++ wp-content/themes/doyle/partials/site-footer.php
@@ -1,20 +1,54 @@
 <footer class="site-footer">
 	<div class="site-footer__inner">
-		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+		<div class="site-footer__item">
+			<p><strong>Contact</strong></p>
 
-		<ul class="social-links">
-		<?php if(get_field('twitter', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('twitter', 'options'); ?>" class="social-links__link"><?php echo get_icon('twitter'); ?></a></li>
-		<?php endif; ?>
-		<?php if(get_field('facebook', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('facebook', 'options'); ?>" class="social-links__link"><?php echo get_icon('facebook'); ?></a></li>
-		<?php endif; ?>
-		<?php if(get_field('instagram', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('instagram', 'options'); ?>" class="social-links__link"><?php echo get_icon('instagram'); ?></a></li>
-		<?php endif; ?>
-		<?php if(get_field('youtube', 'options')): ?>
-			<li class="social-links__item"><a href="<?php the_field('youtube', 'options'); ?>" class="social-links__link"><?php echo get_icon('youtube'); ?></a></li>
-		<?php endif; ?>
-		</ul>
+			<p>T: 01226 298492</p>
+			<p>E: office@doylesecurity.co.uk</p>
+
+			<ul class="social-links">
+			<?php if(get_field('twitter', 'options')): ?>
+				<li class="social-links__item"><a href="<?php the_field('twitter', 'options'); ?>" class="social-links__link"><?php echo get_icon('twitter'); ?></a></li>
+			<?php endif; ?>
+			<?php if(get_field('facebook', 'options')): ?>
+				<li class="social-links__item"><a href="<?php the_field('facebook', 'options'); ?>" class="social-links__link"><?php echo get_icon('facebook'); ?></a></li>
+			<?php endif; ?>
+			<?php if(get_field('instagram', 'options')): ?>
+				<li class="social-links__item"><a href="<?php the_field('instagram', 'options'); ?>" class="social-links__link"><?php echo get_icon('instagram'); ?></a></li>
+			<?php endif; ?>
+			<?php if(get_field('youtube', 'options')): ?>
+				<li class="social-links__item"><a href="<?php the_field('youtube', 'options'); ?>" class="social-links__link"><?php echo get_icon('youtube'); ?></a></li>
+			<?php endif; ?>
+			</ul>
+		</div>
+		<div class="site-footer__item">
+			<p><strong>Services</strong></p>
+			<ul>
+				<li><a href="">Manned Services</a></li>
+				<li><a href="">Electronic Security</a></li>
+			</ul>
+		</div>
+
+		<div class="site-footer__item">
+			<p><strong>Useful links</strong></p>
+			<ul>
+				<li><a href="">FAQ</a></li>
+				<li><a href="">Contact</a></li>
+				<li><a href="">Accreditations & Partners</a></li>
+				<li><a href="">Employee Login</a></li>
+			</ul>
+		</div>
+
+		<div class="site-footer__item">
+			<p><strong>Company</strong></p>
+			<ul>
+				<li><a href="">Complaints Policy</a></li>
+				<li><a href="">Privacy Policy</a></li>
+				<li><a href="">Cookie Policy</a></li>
+			</ul>
+		</div>
+		<div class="site-footer__copy">
+			<p><?php echo date('Y'); ?> &copy; Copyright Doyle Security Ltd Company no. 7806296</p>
+		<div>
 	</div>
 </footer>
