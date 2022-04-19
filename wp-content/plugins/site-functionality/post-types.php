631ccbfd1c6d123a4304b9e304f3119c73add75f
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-04-19 11:10:33 +0100

    Style accordion

--- wp-content/plugins/site-functionality/post-types.php
+++ wp-content/plugins/site-functionality/post-types.php
@@ -1,24 +1,44 @@
 <?php
 
+add_action( 'init', 'cp_change_post_object' );
+// Change dashboard Posts to News
+function cp_change_post_object() {
+    $get_post_type = get_post_type_object('post');
+    $labels = $get_post_type->labels;
+        $labels->name = 'News';
+        $labels->singular_name = 'News';
+        $labels->add_new = 'Add News';
+        $labels->add_new_item = 'Add News';
+        $labels->edit_item = 'Edit News';
+        $labels->new_item = 'News';
+        $labels->view_item = 'View News';
+        $labels->search_items = 'Search News';
+        $labels->not_found = 'No News found';
+        $labels->not_found_in_trash = 'No News found in Trash';
+        $labels->all_items = 'All News';
+        $labels->menu_name = 'News';
+        $labels->name_admin_bar = 'News';
+}
+
 add_action('init', 'origin_cpts');
 
 function origin_cpts() {
-	$post_type_name		= "testimonials";
-	$single_name		= "Testimonial";
-	$plural_name		= "Testimonials";
-	$icon				= "dashicons-format-quote";
-	$gutenberg_enabled	= false;
+	$post_type_name = "case-study";
+	$single_name    = "Case Study";
+	$plural_name    = "Case Studies";
+	$icon           = "dashicons-format-aside";
+	$gutenberg_enabled	= true;
 
 	$post_type_options = array(
 	    'label'                 => $single_name,
-	    'public'                => false,
+	    'public'                => true,
 	    'menu_icon'             => $icon,
 	    'show_ui'               => true,
 	    'show_in_menu'          => true,
 	    'capability_type'       => 'post',
 	    'hierarchical'          => true,
 	    'rewrite'               => array(
-	        'slug'                  => '',
+	        'slug'                  => 'case-study',
 	        'with_front'            => '0'
 	    ),
 	    'query_var'             => true,
@@ -27,13 +47,14 @@ function origin_cpts() {
 	    'supports'              => array(
 	        'title',
 	        'editor',
+			'excerpt',
 	        'revisions',
 	        'thumbnail',
 	        'author',
 	        'page-attributes',
 	    ),
 	    'labels'                => array(
-	        'name'                  => $single_name,
+	        'name'                  => $plural_name,
 	        'singular_name'         => $single_name,
 	        'menu_name'             => $plural_name,
 	        'add_new'               => 'Add ' . $single_name,
@@ -49,14 +70,63 @@ function origin_cpts() {
 	        'parent'                => 'Parent '. $single_name
 	    ),
 	);
-	// register_post_type($post_type_name, $post_type_options);
+	register_post_type($post_type_name, $post_type_options);
+
+	$post_type_name		= "careers";
+	$single_name		= "Job";
+	$plural_name		= "Careers";
+	$icon				= "dashicons-groups";
+	$gutenberg_enabled	= false;
+	
+	$post_type_options = array(
+	    'label'                 => $single_name,
+	    'public'                => true,
+	    'menu_icon'             => $icon,
+	    'show_ui'               => true,
+	    'show_in_menu'          => true,
+	    'capability_type'       => 'post',
+	    'hierarchical'          => true,
+	    'rewrite'               => array(
+	        'slug'                  => 'careers',
+	        'with_front'            => '0'
+	    ),
+	    'query_var'             => true,
+	    'has_archive'           => false,
+		'show_in_rest'			=> $gutenberg_enabled,
+	    'supports'              => array(
+	        'title',
+	        'editor',
+			'excerpt',
+	        'revisions',
+	        'thumbnail',
+	        'author',
+	        'page-attributes',
+	    ),
+	    'labels'                => array(
+	        'name'                  => $plural_name,
+	        'singular_name'         => $single_name,
+	        'menu_name'             => $plural_name,
+	        'add_new'               => 'Add ' . $single_name,
+	        'add_new_item'          => 'Add New ' . $single_name,
+	        'edit'                  => 'Edit',
+	        'edit_item'             => 'Edit ' . $single_name,
+	        'new_item'              => 'New '. $single_name,
+	        'view'                  => 'View '. $single_name,
+	        'view_item'             => 'View '. $single_name,
+	        'search_items'          => 'Search ' . $plural_name,
+	        'not_found'             => 'No '. $plural_name . ' Found',
+	        'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
+	        'parent'                => 'Parent '. $single_name
+	    ),
+	);
+	register_post_type($post_type_name, $post_type_options);
 
 
 
 	$post_type_name		= "team";
 	$single_name		= "Team";
 	$plural_name		= "Teams";
-	$icon				= "dashicons-format-quote";
+	$icon				= "dashicons-admin-users";
 	$gutenberg_enabled	= false;
 
 	$post_type_options = array(
@@ -98,6 +168,54 @@ function origin_cpts() {
 			'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
 			'parent'                => 'Parent '. $single_name
 		),
+	);
+	register_post_type($post_type_name, $post_type_options);
+
+	$post_type_name		= "testimonials";
+	$single_name		= "Testimonial";
+	$plural_name		= "Testimonials";
+	$icon				= "dashicons-format-quote";
+	$gutenberg_enabled	= false;
+
+	$post_type_options = array(
+	    'label'                 => $single_name,
+	    'public'                => true,
+	    'menu_icon'             => $icon,
+	    'show_ui'               => true,
+	    'show_in_menu'          => true,
+	    'capability_type'       => 'post',
+	    'hierarchical'          => true,
+	    'rewrite'               => array(
+	        'slug'                  => '',
+	        'with_front'            => '0'
+	    ),
+	    'query_var'             => true,
+	    'has_archive'           => false,
+		'show_in_rest'			=> $gutenberg_enabled,
+	    'supports'              => array(
+	        'title',
+	        'editor',
+	        'revisions',
+	        'thumbnail',
+	        'author',
+	        'page-attributes',
+	    ),
+	    'labels'                => array(
+	        'name'                  => $single_name,
+	        'singular_name'         => $single_name,
+	        'menu_name'             => $plural_name,
+	        'add_new'               => 'Add ' . $single_name,
+	        'add_new_item'          => 'Add New ' . $single_name,
+	        'edit'                  => 'Edit',
+	        'edit_item'             => 'Edit ' . $single_name,
+	        'new_item'              => 'New '. $single_name,
+	        'view'                  => 'View '. $single_name,
+	        'view_item'             => 'View '. $single_name,
+	        'search_items'          => 'Search ' . $plural_name,
+	        'not_found'             => 'No '. $plural_name . ' Found',
+	        'not_found_in_trash'    => 'No '. $plural_name .' Found in Trash',
+	        'parent'                => 'Parent '. $single_name
+	    ),
 	);
 	// register_post_type($post_type_name, $post_type_options);
 }
--- wp-content/themes/doyle/blocks/doyle/accordion/block.php
+++ wp-content/themes/doyle/blocks/doyle/accordion/block.php
@@ -10,16 +10,20 @@ Allow Multiple: true
 */
 
 $counter = 0;
+$title = get_field('accordion_title');
 $accordion = get_field('accordion_items');
 
 if($accordion) {
 ?>
-<section class="accordion">
+<section class="accordion sp">
+	<?php if ($title) { ?>
+		<h2><?php echo $title; ?></h2>
+	<?php } ?>
 	<?php foreach($accordion as $item) { $counter++; ?>
 		<div class="accordion__item">
 			<button aria-expanded="false" class="accordion__handle" aria-controls="accordion-<?php echo $block['id'] . '_' . $counter; ?>" id="<?php echo sanitize_title($item['accordion_title']); ?>">
-				<h3 class="accordion__title"><?php echo $item['accordion_title']; ?></h3>
-				<div class="accordion__icon">&nbsp;</div>
+				<p class="accordion__title"><?php echo $item['accordion_title']; ?></p>
+				<div class="accordion__icon"><?php echo get_icon('arrow'); ?></div>
 			</button>
 			<div class="accordion__body" id="accordion-<?php echo $block['id'] . '_' . $counter; ?>" role="region" aria-labelledby="<?php echo sanitize_title($item['accordion_title']); ?>">
 				<?php echo $item['accordion_body']; ?>
@@ -28,5 +32,3 @@ if($accordion) {
 	<?php } ?>
 </section>
 <?php } ?>
-
-<a href="http://www.google.com">Link</a>
